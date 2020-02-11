e6bd671d86411393d6a82cdf72ca685da2c91e66
Niranjan Swain <niranjan.Swain@guavus.com>
2020-02-11 15:12:58 +0530

    RAF-1399: retry react module api call if keycloak is enable.

--- cdap-ui/app/cdap/services/datasource/index.js
+++ cdap-ui/app/cdap/services/datasource/index.js
@@ -59,11 +59,10 @@ export default class Datasource {
             the UI and everything loads.
           */
 
-        // TODO RETRY CALL SHOUBD FOR KYCLOAK ENABLE
         let keycloakEnable = window.keycloakEnable != undefined ? window.keycloakEnable : false ;
          let retryCount = this.bindings[hash].count;
-          if ( keycloakEnable && data.statusCode === 401 ) {
-            if(retryCount < 2) {
+          if (data.statusCode === 401 ) {
+            if(keycloakEnable && retryCount < 2) {
               isTokenInvalid = true;
             } else {
               RedirectToLogin({statusCode: 401});
