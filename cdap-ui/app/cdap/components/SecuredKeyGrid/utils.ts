06cd16d6c349691ac9b215a97334d1c54855732a
Anuj Kanojia <>
2020-02-18 12:51:19 +0530

    RAFD-4117: Added copy to clipboard function in util

--- cdap-ui/app/cdap/components/SecuredKeyGrid/utils.ts
+++ cdap-ui/app/cdap/components/SecuredKeyGrid/utils.ts
@@ -1,13 +1,22 @@
-import isNil from 'lodash/isNil';
+import isNil from "lodash/isNil";
 
 export function getEpochDateString(params) {
-    if (!isNil(params)) {
-      if (isNaN(params.value)) {
-        return "—";
-      } else {
-        const date = new Date(params.value * 1000);
-        return date.toDateString();
-      }
+  if (!isNil(params)) {
+    if (isNaN(params.value)) {
+      return "—";
+    } else {
+      const date = new Date(params.value * 1000);
+      return date.toDateString();
     }
-    return "—";
   }
+  return "—";
+}
+
+export function copyToClipboard(clipboardText) {
+  const textArea = document.createElement("textarea");
+  textArea.value = clipboardText;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
+}
